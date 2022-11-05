71f315f79cb679bbc41eafa527a9c30b8dcda3ab
Adeyemi <yemiklein77@gmail.com>
2022-11-05 21:56:21 +0100

    new commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <p>How would you React if i say I 🧡 Vue!</p>
+ </div>
+  );
+}
+
+export default App;
