1c64acef664b09aa1de4c4a6cdba2a0736dab12a
Adeyemi <yemiklein77@gmail.com>
2022-11-06 02:02:23 +0100

    next commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,19 @@ import Signup from "./components/Signup";
 import Dashboard from "./components/Dashboard";
 import PhoneVerify from "./components/PhoneVerify";
 
+
+
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Login />} />
-                <Route path='/register' element={<Signup />} />
-                <Route path='/dashboard' element={<Dashboard />} />
-                <Route path='phone/verify' element={<PhoneVerify />} />
-            </Routes>
-        </BrowserRouter>
-    );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Login />} />
+				<Route path='/register' element={<Signup />} />
+				<Route path='/dashboard' element={<Dashboard />} />
+				<Route path='phone/verify' element={<PhoneVerify />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -16,96 +16,85 @@ app.use(cors());
 
 
 
-//👇🏻 Generates the code to be sent via SMS
+const users = [];
+let code;
+
+const generateID = () => Math.random().toString(36).substring(2, 10);
 const generateCode = () => Math.random().toString(36).substring(2, 12);
 
 const sendNovuNotification = async (recipient, verificationCode) => {
-    try {
-        let response = await novu.trigger("<NOTIFICATION_TEMPLATE_ID>", {
-            to: {
-                subscriberId: recipient,
-                phone: recipient,
-            },
-            payload: {
-                code: verificationCode,
-            },
-        });
-        console.log(response);
-    } catch (err) {
-        console.error(err);
-    }
+	try {
+		let response = await novu.trigger("<NOTIFICATION_TEMPLATE>", {
+			to: {
+				subscriberId: recipient,
+				phone: recipient,
+			},
+			payload: {
+				code: verificationCode,
+			},
+		});
+		console.log(response);
+	} catch (err) {
+		console.error(err);
+	}
 };
 
+app.post("/api/login", (req, res) => {
+	const { email, password } = req.body;
+	let result = users.filter(
+		(user) => user.email === email && user.password === password
+	);
 
+	if (result.length !== 1) {
+		return res.json({
+			error_message: "Incorrect credentials",
+		});
+	}
+	code = generateCode();
+	/* 
+		👇🏻 Log the user's number and the code sent 
+		to the console during development
+	*/
+	//console.log("Telephone Number", result[0].tel);
+	//console.log(`Generated code is ${code}`);
 
+	//👇🏻 Send the SMS via Novu
+	sendNovuNotification(result[0].tel, code);
 
-
-//👇🏻 Generates a random string as the ID
-const generateID = () => Math.random().toString(36).substring(2, 10);
-
-
-const postLoginDetails = () => {
-    fetch("http://localhost:4000/api/login", {
-        method: "POST",
-        body: JSON.stringify({
-            email,
-            password,
-        }),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((err) => console.error(err));
-};
-const handleSubmit = (e) => {
-    e.preventDefault();
-    //👇🏻 Calls the function
-    postLoginDetails();
-    setPassword("");
-    setEmail("");
-};
-
-
+	res.json({
+		message: "Login successfully",
+		data: {
+			username: result[0].username,
+		},
+	});
+});
 
 app.post("/api/register", (req, res) => {
-    //👇🏻 Get the user's credentials
-    const { email, password, tel, username } = req.body;
-
-    //👇🏻 Checks if there is an existing user with the same email or password
-    let result = users.filter((user) => user.email === email || user.tel === tel);
-
-    //👇🏻 if none
-    if (result.length === 0) {
-        //👇🏻 creates the structure for the user
-        const newUser = { id: generateID(), email, password, username, tel };
-        //👇🏻 Adds the user to the array of users
-        users.push(newUser);
-        //👇🏻 Returns a message
-        return res.json({
-            message: "Account created successfully!",
-        });
-    }
-    //👇🏻 Runs if a user exists
-    res.json({
-        error_message: "User already exists",
-    });
+	const { email, password, tel, username } = req.body;
+	let result = users.filter((user) => user.email === email || user.tel === tel);
+	if (result.length === 0) {
+		const newUser = { id: generateID(), email, password, username, tel };
+		users.push(newUser);
+		return res.json({
+			message: "Account created successfully!",
+		});
+	}
+	res.json({
+		error_message: "User already exists",
+	});
 });
 
-
-// "subscriberId": "6366db059e04bb11b54efab6",
-// +14793411973 twillo
-// ACa2de2b0e0cfb59d015eb1058884cceb9 SID
-// c9a125cd0f7dec0ea8402cd3610fcf15 AUTH TOKEN
+app.post("/api/verification", (req, res) => {
+	if (code === req.body.code) {
+		return res.json({ message: "You're verified successfully" });
+	}
+	res.json({
+		error_message: "Incorrect credentials",
+	});
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT} 🪐🚀`);
+	console.log(`Server listening live on ${PORT} 🪐🚀`);
 });
 
-
-                  
-
-                  
+              
